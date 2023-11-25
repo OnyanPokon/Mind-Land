48af5efab82b63dc3f1166f52bf3fbb0fee8f2f1
Dikymonz <diky.monz03@gmail.com>
2023-11-25 11:21:25 +0800

    optimize responsive

--- src/pages/CheckNow/CheckNow.jsx
+++ src/pages/CheckNow/CheckNow.jsx
@@ -1,16 +1,18 @@
 import React from "react";
+
 import CheckNowHero from "../../assets/logofitur.json";
-import Lottie from "lottie-react";
+import StressIcon from "../../assets/img/icons/stressIcon.json";
+import AnxietyIcon from "../../assets/img/icons/anxietyIcon.json";
+import DepressionIcon from "../../assets/img/icons/depressionIcon.json";
+import TraumaIcon from "../../assets/img/icons/traumaIcon.json";
+import PsychologIcon from "../../assets/img/icons/psychologistIcon.json";
+
+import Lottie, { LottiePlayer } from "lottie-react";
 import { BsChevronDoubleRight } from "react-icons/bs";
-
 import { Container, Row, Col, Button } from "react-bootstrap";
-
 import { useNavigate } from "react-router-dom";
 
 import "../../style/checknow.css";
-
-// import FaqComponent from "../../components/FaqComponent";
-// import CardComponent from "../../components/CardComponent";
 
 const CheckNow = () => {
   let naviget = useNavigate();
@@ -23,39 +25,41 @@ const CheckNow = () => {
               <h1 className="mt-5">
                 Loyal companion in every journey of your life.
               </h1>
-              <div className="checknow-card mt-4">
-                <img
-                  className="card-image"
-                  src="https://via.placeholder.com/150"
-                  alt="fiture picture"
-                />
-                <h2 className="checknow-title">Stress</h2>
+              <div className="checknow-card-group mt-3">
+                <div className="checknow-card">
+                  <Lottie
+                    className="card-image"
+                    animationData={StressIcon}
+                    alt="fiture picture"
+                  />
+                  <h2 className="checknow-title">Stress</h2>
+                </div>
+                <div className="checknow-card">
+                  <Lottie
+                    className="card-image"
+                    animationData={AnxietyIcon}
+                    alt="fiture picture"
+                  />
+                  <h2 className="checknow-title">Gangguan Kecemasan</h2>
+                </div>
+                <div className="checknow-card">
+                  <Lottie
+                    className="card-image"
+                    animationData={DepressionIcon}
+                    alt="fiture picture"
+                  />
+                  <h2 className="checknow-title">Depresi</h2>
+                </div>
+                <div className="checknow-card">
+                  <Lottie
+                    className="card-image"
+                    animationData={TraumaIcon}
+                    alt="fiture picture"
+                  />
+                  <h2 className="checknow-title">Trauma</h2>
+                </div>
               </div>
-              <div className="checknow-card">
-                <img
-                  className="card-image"
-                  src="https://via.placeholder.com/150"
-                  alt="fiture picture"
-                />
-                <h2 className="checknow-title">Kecemasan</h2>
-              </div>
-              <div className="checknow-card">
-                <img
-                  className="card-image"
-                  src="https://via.placeholder.com/150"
-                  alt="fiture picture"
-                />
-                <h2 className="checknow-title">Depresi</h2>
-              </div>
-              <div className="checknow-card">
-                <img
-                  className="card-image"
-                  src="https://via.placeholder.com/150"
-                  alt="fiture picture"
-                />
-                <h2 className="checknow-title">Trauma</h2>
-              </div>
-              <div className="desc">
+              <div className="desc mt-3">
                 <h1>
                   Stress is not always bad, but long-term stress can disrupt
                   your health. Various situations or life events can trigger
@@ -64,21 +68,18 @@ const CheckNow = () => {
                 <Button
                   variant="info"
                   size="lg"
-                  className="mt-4 btn-md text-light center rounded-2"
+                  className="mt-3 btn-md text-light center rounded-2"
                   onClick={() => naviget("/doctorcard")}
                 >
                   {" "}
                   Find Expert{" "}
                 </Button>
               </div>
-              <div className="wrapper mt-5">
+              <div className="wrapper mt-5 d-flex">
                 <div className="card-fitur">
                   <div className="content">
                     <div className="img">
-                      <img
-                        src="https://media.istockphoto.com/id/489951194/id/vektor/pikiran-manusia-dengan-teka-teki-jigsaw.jpg?s=612x612&w=0&k=20&c=4QLFFW89dIH9rooXxPnDPbYsPaih9pHIOnGLNjSu0OU="
-                        alt=""
-                      />
+                      <Lottie animationData={PsychologIcon} alt="" />
                     </div>
                     <div className="detail">
                       <span className="name">Pilih Psikolog</span>
@@ -93,7 +94,7 @@ const CheckNow = () => {
             </Col>
             <Col lg="6" className="pt-lg-0 pt-5 d-flex justify-content-end">
               <Lottie
-                className="checknow-img"
+                className="checknow-img mb-5"
                 animationData={CheckNowHero}
                 alt="checknow-image"
               />
